eff458fa3941f391a8664252d8ca7d6e0d20b2a5
0xe82de@gmail.com <0xe82de@gmail.com>
2021-10-19 23:32:10 +0900

    greetings

---
+++ js/greetings.js
@@ -0,0 +1,33 @@
+const loginForm = document.querySelector("#login-form");
+const loginInput = document.querySelector("#login-form input");
+const greeting = document.querySelector("#greeting");
+
+const HIDDEN_CLASSNAME = "hidden";
+const USERNAME_KEY = "username";
+
+// submit 이벤트가 발생하면 브라우저는 새로고침을 한다.
+// preventDefault()로 이것을 막을 수 있다.
+function onLoginSubmit(event) {
+  event.preventDefault();
+  loginForm.classList.add(HIDDEN_CLASSNAME);
+  const username = loginInput.value;
+  localStorage.setItem(USERNAME_KEY, username);
+  paintGreetings();
+}
+
+function paintGreetings(username) {
+  greeting.innerText = `Hello ${username}`;
+  greeting.classList.remove(HIDDEN_CLASSNAME);
+}
+
+const savedUsername = localStorage.getItem(USERNAME_KEY);
+
+if (savedUsername === null) {
+  // show the form
+  loginForm.classList.remove(HIDDEN_CLASSNAME);
+  loginForm.addEventListener("submit", onLoginSubmit);
+}
+else {
+  // show the greetings
+  paintGreetings(savedUsername);
+}
